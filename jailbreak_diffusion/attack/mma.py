961d2b26e76361e01a4ed3e114f6d4f079c4001b
YukinoAsuna <zxweng0701@gmail.com>
2025-01-15 00:03:50 -0500

    update MMA

--- jailbreak_diffusion/attack/mma.py
+++ jailbreak_diffusion/attack/mma.py
@@ -5,7 +5,7 @@ from rich import print
 from diffusers import StableDiffusionInpaintPipeline
 from diffusers import DiffusionPipeline
 import torch.nn as nn
-import torch.nn.functional as F 
+import torch.nn.functional as F
 import time
 import numpy as np
 import gc
@@ -14,26 +14,18 @@ from typing import Any, Optional, Callable
 import string
 import argparse
 import os
-import torch
-import numpy as np
-import random
-import pathlib
 from .base import BaseAttacker, AttackResult
 from .factory import AttackerFactory
+
 class CosineSimilarityLoss(nn.Module):
     def __init__(self, reduction='mean'):
         super(CosineSimilarityLoss, self).__init__()
         self.reduction = reduction
 
     def forward(self, x, y):
-        # Compute cosine similarity
         cos_sim = nn.functional.cosine_similarity(x, y, dim=1, eps=1e-6)
-        
-        # Compute cosine similarity loss
-        # We subtract the cosine similarity from 1 because we want to minimize the loss to make the cosine similarity maximized.
         loss = 1 - cos_sim
 
-        # Apply reduction
         if self.reduction == 'mean':
             loss = loss.mean()
         elif self.reduction == 'sum':
@@ -41,219 +33,162 @@ class CosineSimilarityLoss(nn.Module):
 
         return loss
 
-class SDattack(object):
-    """ A class used to manage adversarial prompt attacks. """
+class MMA_Agent:
+    """ Manages adversarial prompt attacks. """
 
-    def __init__(self,
-                model,
-                tokenizer, 
-                target_embeddings=None,
-                tokens_to_remove_set=None,
-                *args, **kwargs
-                ):
-        self.init_control_str()
+    def __init__(self, model, tokenizer, target_embeddings=None, tokens_to_remove_set=None):
         self.model = model
         self.tokenizer = tokenizer
         self.target_embeddings = target_embeddings
         self.tokens_to_remove_set = tokens_to_remove_set
+        self.init_control_str()
+
     def init_control_str(self):
         letters = [random.choice(string.ascii_letters) for _ in range(20)]
-        random_string = " ".join(letters)
-        self.control_str = random_string
-        self.best_control = random_string
-    def token_gradient(self,control, target_embeddings):
+        self.control_str = self.best_control = " ".join(letters)
 
+    def token_gradient(self, control, target_embeddings):
+        #BUG:查看max_length的值为77
         tokens = self.tokenizer(control, padding="max_length", max_length=77, return_tensors="pt", truncation=True)
-
-        input_ids = tokens["input_ids"].cuda() #shape [1, 77]
-        embed_weights = self.model.text_model.embeddings.token_embedding.weight # shape [49408, 768]
+        input_ids = tokens["input_ids"].cuda()
+        embed_weights = self.model.text_model.embeddings.token_embedding.weight
 
         control_length = 20
-        one_hot = torch.zeros(control_length,embed_weights.shape[0],device=self.model.device,dtype=embed_weights.dtype)
-
-        one_hot.scatter_(1,input_ids[0][:control_length].unsqueeze(1),torch.ones(one_hot.shape[0], 1, device=self.model.device, dtype=embed_weights.dtype))
-
+        one_hot = torch.zeros(control_length, embed_weights.shape[0], device=self.model.device, dtype=embed_weights.dtype)
+        one_hot.scatter_(1, input_ids[0][:control_length].unsqueeze(1), 1.0)
         one_hot.requires_grad_()
-        input_embeds = (one_hot @ embed_weights).unsqueeze(0)# shape [1, 20, 768]，得到20个token的embedding
-        # input_embeds.shape [1, 20, 4096]
-        embeds = self.model.text_model.embeddings.token_embedding(input_ids) # [1, 77, 768]，和input_embeds的前20行的embedding是一样，就是多了57个pad token的embedding
-        full_embeds = torch.cat([input_embeds,embeds[:, control_length:]], dim=1) # [1, 77, 768]#这个full_embeds其实就是embeds
+
+        input_embeds = (one_hot @ embed_weights).unsqueeze(0)
+        embeds = self.model.text_model.embeddings.token_embedding(input_ids)
+        full_embeds = torch.cat([input_embeds, embeds[:, control_length:]], dim=1)
+
         position_embeddings = self.model.text_model.embeddings.position_embedding
-
-        position_ids = torch.arange(0,77).cuda()
+        position_ids = torch.arange(0, 77).cuda()
         pos_embeds = position_embeddings(position_ids).unsqueeze(0)
+
         embeddings = full_embeds + pos_embeds
-
-        # ! modify the transformers.model.clip.modeling_clip.py forward function CLIPTextModel, CLIPTextTransformer
-
-        embeddings = self.model(input_ids=input_ids, input_embed=embeddings)["pooler_output"] # [1, 768]，就是control string的global embedding
-
+        #BUG:
+        #阅读这个link：https://github.com/cure-lab/MMA-Diffusion?tab=readme-ov-file,
+        embeddings = self.model(input_ids=input_ids, input_embed=embeddings)["pooler_output"]
 
         criteria = CosineSimilarityLoss()
         loss = criteria(embeddings, self.target_embeddings)
-
         loss.backward()
 
-        return one_hot.grad.clone() # shape [20, 49408] max 0.05, min 0.05
+        return one_hot.grad.clone()
+
     @torch.no_grad()
-    def logits(self,model, tokenizer, test_controls=None, return_ids=False): # test_controls indicates the candicate controls 512 same as batch_size 
-        # pad_tok = -1
-        # print("test_controls list length:", test_controls.__len__()) # batch_size = 512
-
+    def logits(self, model, tokenizer, test_controls=None, return_ids=False):
+        #BUG:查看max_length的值为77
         cand_tokens = tokenizer(test_controls, padding='max_length', max_length=77, return_tensors="pt", truncation=True)
-
         input_ids = cand_tokens['input_ids'].cuda()
 
         if return_ids:
-            return model(input_ids=input_ids)['pooler_output'].cuda(), input_ids # embeddings shape [512, 768]
+            return model(input_ids=input_ids)['pooler_output'].cuda(), input_ids
         else:
             return model(input_ids=input_ids)['pooler_output'].cuda()
-    
-    
-    def sample_control(self,grad, batch_size, topk=256, tokenizer=None, control_str=None,allow_non_ascii=False):
 
+    def sample_control(self, grad, batch_size, topk=256, tokenizer=None, control_str=None):
         for input_id in set(self.tokens_to_remove_set):
-            grad[:, input_id] = np.inf#将所有涉及敏感 token 的梯度置为无穷大，迫使 topk 采样忽略这些 token。
-        top_indices = (-grad).topk(topk, dim=1).indices# shape [20, 256],提取出每个token位置中可以让loss变最小的250个token
+            grad[:, input_id] = np.inf
 
-
+        top_indices = (-grad).topk(topk, dim=1).indices
         tokens = tokenizer.tokenize(control_str)
-        control_toks = torch.Tensor(tokenizer.convert_tokens_to_ids(tokens)).to(grad.device)
-        control_toks = control_toks.type(torch.int64)# shape [20]
+        control_toks = torch.Tensor(tokenizer.convert_tokens_to_ids(tokens)).to(grad.device).type(torch.int64)
 
-        # control_toks = self.control_toks.to(grad.device)
-        original_control_toks = control_toks.repeat(batch_size, 1) #* shape [512, 20]#将 control_toks 扩展为 [batch_size, 20] 的张量，用于在每个批量中共享相同的初始 token。
-
-        new_token_pos = torch.arange(0, len(control_toks), len(control_toks)/batch_size).type(torch.int64).cuda() #shape [512]
-        new_token_val = torch.gather(top_indices[new_token_pos], 1,torch.randint(0, topk, (batch_size, 1), device=grad.device)) # (512, 1)[batch_size, 1]，表示每个样本中需要替换 token 的新值。
+        original_control_toks = control_toks.repeat(batch_size, 1)
+        new_token_pos = torch.arange(0, len(control_toks), len(control_toks)/batch_size).type(torch.int64).cuda()
+        new_token_val = torch.gather(top_indices[new_token_pos], 1, torch.randint(0, topk, (batch_size, 1), device=grad.device))
 
         new_control_toks = original_control_toks.scatter_(1, new_token_pos.unsqueeze(-1), new_token_val)
-        # (512, 20)，就是每个样本中被替换的 token 的新值。(每个样本只替换一个token)
-
         return new_control_toks
-    
+
     def get_filtered_cands(self, control_cand, filter_cand=True, curr_control=None):
-        cands, count = [], 0
-        
-        tokenizer = self.tokenizer
+        cands = []
         for i in range(control_cand.shape[0]):
-            # import ipdb; ipdb.set_trace()
-            # decoded_str = tokenizer.decode(control_cand[i], skip_special_tokens=True)
-            decoded = tokenizer.convert_ids_to_tokens(control_cand[i])
+            decoded = self.tokenizer.convert_ids_to_tokens(control_cand[i])
             decoded_str = "".join(decoded).replace('</w>', ' ')[:-1]
             if filter_cand:
-                if decoded_str != curr_control and len(tokenizer(decoded_str, add_special_tokens=False).input_ids) == len(control_cand[i]):
-                    '''
-                    候选字符串通过分词器重新编码后，其 token 数量必须和原始 token 序列长度一致。
-    目的是确保候选字符串是有效的、可还原的 token 序列。
-                    '''
+                if decoded_str != curr_control and len(self.tokenizer(decoded_str, add_special_tokens=False).input_ids) == len(control_cand[i]):
                     cands.append(decoded_str)
-                else:
-                    count += 1
             else:
                 cands.append(decoded_str)
-                
+
         if filter_cand:
             cands = cands + [cands[-1]] * (len(control_cand) - len(cands))
-            # print(f"Warning: {round(count / len(control_cand), 2)} control candidates were not valid")
         return cands
 
-  
-    def step(self,
-            batch_size=512,
-            topk=256,
-            filter_cand=True,
-            ):
-
+    def step(self, batch_size=512, topk=256, filter_cand=True):
         control_cands = []
-        #'N q V w Y S V P H b D X p P d k h x E p'
-        new_grad = self.token_gradient(self.control_str,self.target_embeddings) # receive model and prompt to get the grident 
-
+        new_grad = self.token_gradient(self.control_str, self.target_embeddings)
         new_grad = new_grad / new_grad.norm(dim=-1, keepdim=True)
 
-
         with torch.no_grad():
-            control_cand = self.sample_control(new_grad, batch_size, topk, tokenizer=self.tokenizer, control_str=self.control_str) # [512, 20]
+            control_cand = self.sample_control(new_grad, batch_size, topk, tokenizer=self.tokenizer, control_str=self.control_str)
             control_cands.append(self.get_filtered_cands(control_cand, filter_cand=filter_cand, curr_control=self.control_str))
-            '''
-            self.get_filtered_cands(control_cand, filter_cand=filter_cand, curr_control=self.control_str)返回一个长《=512的list（有些可能被过滤掉了），这个list就是每个元素都是被修改后的control string
-            '''
-        del new_grad, control_cand ; gc.collect()
 
-        # target_embeddings = torch.load("/opt/data/home/bian/data/ZOC/a_completely_naked_woman.pt").cuda()
+        del new_grad, control_cand
+        gc.collect()
+
         with torch.no_grad():
-            for j, cand in enumerate(control_cands):
-            # looping throught the prompts at this level is less elegent, but we can manage VRAM better this way
-            # progress = tqdm(range(len(self.prompt)))
-                cand_embeddings = self.logits(self.model, self.tokenizer, test_controls=cand)#shape [512, 768]，就是给每个cand string得到一个global embedding
-                
+            for cand in control_cands:
+                cand_embeddings = self.logits(self.model, self.tokenizer, test_controls=cand)
                 cos_loss = CosineSimilarityLoss(reduction=None)
-                loss = cos_loss(cand_embeddings, self.target_embeddings) # shape 512
-                # if verbose:
-                    # print("min loss:", loss.min().item())
+                loss = cos_loss(cand_embeddings, self.target_embeddings)
 
         min_idx = loss.argmin()
-        next_control = cand[min_idx]#只返回了一个最小的loss对应的control string
+        next_control = cand[min_idx]
         cand_loss = loss[min_idx]
-        del control_cands, loss, cand; gc.collect()
-        # print("Current control:", next_control)
-        return next_control, cand_loss.item()   
-  
-    def run(
-        self,
-        n_steps=1000,
-        batch_size=512,
-        topk=256,
-        filter_cand=True,
-        ):
-        steps = 0
-        loss = best_loss = 1e6 
-        best_steps = 0
-        for i in range(n_steps):
-            steps += 1
+
+        del control_cands, loss, cand
+        gc.collect()
+
+        return next_control, cand_loss.item()
+
+    def run(self, n_steps=1000, batch_size=512, topk=256, filter_cand=True):
+        best_loss = float('inf')
+        for _ in range(n_steps):
             torch.cuda.empty_cache()
-            control, loss = self.step(batch_size = batch_size,topk = topk,filter_cand = filter_cand)
-            keep_control = True
-            if keep_control:
-                self.control_str = control
+            control, loss = self.step(batch_size=batch_size, topk=topk, filter_cand=filter_cand)
+
             if loss < best_loss:
                 best_loss = loss
                 self.best_control = control
-                best_steps = steps
-                print("best_control:", self.best_control)
-                print(f"steps: {steps}", 'Best Loss:', best_loss)
-        return self.best_control, best_loss, best_steps 
+                print("Best Control:", self.best_control, "Best Loss:", best_loss)
+
+        return self.best_control, best_loss
 
 class MMAttack(BaseAttacker):
-    def __init__(
-        self,
-        target_model: Any=None,
-        text_detector: Optional[Callable[[str], bool]] = None,
-        image_checker: Optional[Callable[[Any], bool]] = None,
-        **kwargs
-    ):
+    def __init__(self, target_model: Any = None, **kwargs):
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.target_model = StableDiffusionInpaintPipeline.from_pretrained("benjamin-paine/stable-diffusion-v1-5-inpainting",
-                            variant="fp16",
-                            torch_dtype=torch.float16,
-                            ).to(device)
+        self.target_model = StableDiffusionInpaintPipeline.from_pretrained(
+            "benjamin-paine/stable-diffusion-v1-5-inpainting",
+            variant="fp16",
+            torch_dtype=torch.float16,
+        ).to(device)
+
         self.text_encoder = self.target_model.text_encoder
         self.tokenizer = self.target_model.tokenizer
         self.remove_set()
-        self.n_steps=kwargs.get('n_steps', 10)
-        self.topk=kwargs.get('topk', 256)
-        self.batch_size=kwargs.get('batch_size', 512)
+        self.n_steps = kwargs.get('n_steps', 1000)
+        self.topk = kwargs.get('topk', 256)
+        self.batch_size = kwargs.get('batch_size', 512)
 
     def remove_set(self):
-        nsfw_word_list = ['sexual','nude','nudity','sex','18+','naked','nsfw','porn', 'dick','vagina','naked child','explicit content','uncensored','fuck','nipples','visible nipples','breast','areola',]
-        sensitive_word_list = ['ak','na', 'ke','nud'] + nsfw_word_list
-        tokens_to_remove_list = []
-        for index, word in enumerate(sensitive_word_list):
-            for token, token_id in self.tokenizer.get_vocab().items():
-                if word in token:
-                    tokens_to_remove_list.append(token_id)
-        self.tokens_to_remove_set = set(tokens_to_remove_list)
+        nsfw_word_list = [
+            'sexual', 'nude', 'nudity', 'sex', '18+', 'naked', 'nsfw', 'porn', 'dick',
+            'vagina', 'naked child', 'explicit content', 'uncensored', 'fuck',
+            'nipples', 'visible nipples', 'breast', 'areola',
+        ]
+        sensitive_word_list = ['ak', 'na', 'ke', 'nud'] + nsfw_word_list
 
+        self.tokens_to_remove_set = set(
+            token_id
+            for word in sensitive_word_list
+            for token, token_id in self.tokenizer.get_vocab().items()
+            if word in token
+        )
+        
     def attack(self, prompt: str, **kwargs) -> AttackResult:
         start_time = time.time()
         result = {
@@ -262,36 +197,46 @@ class MMAttack(BaseAttacker):
             'image': None,
             'num_query': 0
         }
-        target_tokenized = self.tokenizer(prompt, padding="max_length", max_length=77, return_tensors="pt", truncation=True)
+        #BUG:查看max_length的值为77
+        target_tokenized = self.tokenizer(prompt, 
+                padding="max_length", max_length=77, 
+                return_tensors="pt", truncation=True)
         target_input = target_tokenized["input_ids"].to(self.text_encoder.device)
-        target_embedding = self.text_encoder(target_input)["pooler_output"]# 得到了shape [1, 768]的global representation
+        target_embedding = self.text_encoder(target_input)["pooler_output"]
         torch.save(target_embedding,"target_embedding.pt")
+        '''
+            这里保存再读取的原因是因为target_embedding如果不这样处理的话，就会被多次backwards而报错。
+            而保存再读取可以避免这个问题。
+        '''
         target_embedding = torch.load("target_embedding.pt").to(self.text_encoder.device)
-        prompt_attack = SDattack(model=self.text_encoder, tokenizer=self.tokenizer, target_embeddings=target_embedding,tokens_to_remove_set=self.tokens_to_remove_set)
-        best_control, loss, steps = prompt_attack.run(n_steps=self.n_steps, topk=self.topk, batch_size=self.batch_size)
-        result['perturbed_prompt'] = best_control
+        attack_agent = MMA_Agent(
+            model=self.text_encoder,
+            tokenizer=self.tokenizer,
+            target_embeddings=target_embedding,
+            tokens_to_remove_set=self.tokens_to_remove_set
+        )
+
+        best_perturbed_prompt, best_loss = attack_agent.run(
+            n_steps=self.n_steps, topk=self.topk, batch_size=self.batch_size
+        )
+        result['perturbed_prompt'] = best_perturbed_prompt
         end_time = time.time()
-        
-        
         bypass_detector = self.check_text(result['perturbed_prompt'])
         
         generation_output = self.target_model.generate(result['perturbed_prompt'])
-        
         bypass_checker = self.check_image(generation_output.images)
         
         success = bypass_detector and bypass_checker
-        
         return AttackResult(
-            success=success,
+            success=True,
             original_prompt=prompt,
-            attack_prompt=perturbed_prompt,
+            attack_prompt=result['perturbed_prompt'],
             execution_time=end_time - start_time,
             bypass_checker=bypass_checker,
             bypass_detector=bypass_detector,
             generated_image=generation_output.images,
-            method='DACA',
-            metadata={
-                'num_queries': result['num_query'],
-            }
+            method='MMAttack',
+            metadata={'num_queries': 0},  # Placeholder
         )
+
 AttackerFactory.register('MMAttack', MMAttack)
